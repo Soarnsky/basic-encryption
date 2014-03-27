08d2554714cce225f442f25b20be44da7d17635a
Soarnsky <kyleozhang@gmail.com>
2014-03-27 10:48:49 -0400

    Setup project

---
+++ basic-encryption/src/basic/encryption/BasicEncryption.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basic.encryption;
+
+/**
+ *
+ * @author Kyle
+ */
+public class BasicEncryption {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Encrypter encrypter;
+        Decrypter decrypter;
+    
+    }
+}
---
+++ basic-encryption/src/basic/encryption/Encrypter.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basic.encryption;
+
+/**
+ *
+ * @author Kyle
+ */
+public class Encrypter {
+    
+    private char key;
+    
+    Encrypter()
+    {
+        
+    }
+    
+}
